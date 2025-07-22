695e8ebf80e35daf3373036256a7163f10fcad05
Muhammed Eryılmaz <muherylmaz@gmail.com>
2025-07-22 22:24:33 +0300

    Mesh hataları düzeltildi

---
+++ My project (7)/Assets/Scripts/Kayıt/KayıtVerileri.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class KayýtVerileri
+{
+    public float konumX;
+    public float konumY;
+    public float konumZ;
+
+    // Görev ve krallýk iliþkileri eklenecek
+}
+
